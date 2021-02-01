dde6136ba0c0780c51ad66148f73bd7956c85c15
imanardiansyahh <imanardiansyah89@gmail.com>
2021-02-01 22:05:56 +0800

    malam kedua

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // async  code example
 
-
-console.log("Start")
+console.log("Start");
 
 // setTimeout (() =>{
 //     console.log("we are the time out")
@@ -13,16 +12,74 @@ console.log("Start")
 // })
 
 function loginUser(email, password, callback) {
-    setTimeout(()=> {
-        console.log("ini datanya")
-        callback({userEmail : email, userPassword : password})
-    }, 4000)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("ini datanya");
+      resolve({ userEmail: email, userPassword: password });
+    }, 3000);
+  });
+}
+
+function getUserVideos(email, cb) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["videos1", "videos2", "videos3"]);
+    }, 2000);
+  });
+}
+
+function videoDetails(video, callback) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("title of the video");
+    }, 2000);
+  });
 }
 
 // <-- callback-->
- const user = loginUser("imanardiansyah@gmail.com", 123456, user => {
-     console.log(user)
- })
+// const user = loginUser("imanardiansyah@gmail.com", 123456, (user) => {
+//   console.log(user);
+//   getUserVideos(user.userEmail, (videos) => {
+//     console.log(videos);
+//     videoDetails(videos,title => {
+//       console.log(title)
+//     })
+//   });
+// });
 
+// loginUser('ed','bubmba')
+// .then(user => getUserVideos(user.email))
+// .then(videos => videoDetails(videos[0]))
+// .then(detail =>console.log(detail))
 
-console.log("Finish")
+async function displayUser() {
+  try {
+    const loggedUser = await loginUser("ed", 12345);
+    const videos = await getUserVideos(loggedUser.userEmail);
+    const detail = await videoDetails(videos[0]);
+    console.log(detail);
+  }
+  catch (error){
+    console.log('we could not get the video')
+  }
+}
+displayUser();
+
+console.log("Finish");
+
+// sync
+// const yt = new Promise (resolve => {
+//   setTimeout (() => {
+//     console.log('getting start from yputube')
+//     resolve({videos : [1, 2, 3, 4, 5]})
+//   },2000)
+// })
+
+// const fb = new Promise (resolve => {
+//   setTimeout (() => {
+//     console.log('stuff from fb')
+//     resolve({user : "name"})
+//   },2000)
+// })
+
+// Promise.all([yt, fb]).then(resulu => console.log(resulu))
---
+++ promise.js
@@ -0,0 +1,12 @@
+const promise = new Promise ((resolve, reject) => {
+    setTimeout(() => {
+        console.log('got the user')
+resolve({user: "iman"})
+reject(new Error("user not logged in"))
+    }, 2000)
+})
+
+promise.then(user =>{
+    console.log(user)
+})
+.catch(err => console.log(err.message))
